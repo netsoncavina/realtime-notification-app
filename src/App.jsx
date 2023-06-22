25af835cd76b7ecb6b01e898832cca8ddde00352
Netson Cavina <netsoncavina@gmail.com>
2023-06-22 20:51:33 -0300

    Controle de estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  // console.log(user);
   return (
     <div className="container">
       <div className="login">
-        <input type="text" placeholder="Username" />
-        <button>Login</button>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={() => setUser(username)}>Login</button>
       </div>
     </div>
   );
