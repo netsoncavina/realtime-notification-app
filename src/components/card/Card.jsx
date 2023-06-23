86ae8bfb978d11fce12386704cc10a86fe6d9de9
Netson Cavina <netsoncavina@gmail.com>
2023-06-22 21:16:33 -0300

    Adicionado função de like

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./card.css";
 import Heart from "../../img/heart.svg";
 import HeartFilled from "../../img/heartFilled.svg";
@@ -7,6 +7,7 @@ import Share from "../../img/share.svg";
 import Info from "../../img/info.svg";
 
 export const Card = ({ post }) => {
+  const [liked, setLiked] = useState(false);
   return (
     <div>
       <div className="info">
@@ -15,7 +16,21 @@ export const Card = ({ post }) => {
       </div>
       <img src={post.postImg} alt="" className="postImg" />
       <div className="interaction">
-        <img src={Heart} alt="" className="cardIcon" />
+        {liked ? (
+          <img
+            src={HeartFilled}
+            alt=""
+            className="cardIcon"
+            onClick={() => setLiked(!liked)}
+          />
+        ) : (
+          <img
+            src={Heart}
+            alt=""
+            className="cardIcon"
+            onClick={() => setLiked(!liked)}
+          />
+        )}
         <img src={Comment} alt="" className="cardIcon" />
         <img src={Share} alt="" className="cardIcon" />
         <img src={Info} alt="" className="cardIcon" />
