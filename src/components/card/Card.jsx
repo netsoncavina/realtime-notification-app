f89d2dde6af5417208d2e74020fd17d6dfa90e3e
Netson Cavina <netsoncavina@gmail.com>
2023-06-22 21:12:51 -0300

    Desenvolvimento do Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./card.css";
+import Heart from "../../img/heart.svg";
+import HeartFilled from "../../img/heartFilled.svg";
+import Comment from "../../img/comment.svg";
+import Share from "../../img/share.svg";
+import Info from "../../img/info.svg";
 
-export const Card = () => {
-  return <div>Card</div>;
+export const Card = ({ post }) => {
+  return (
+    <div>
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fullName}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interaction">
+        <img src={Heart} alt="" className="cardIcon" />
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon" />
+      </div>
+    </div>
+  );
 };
