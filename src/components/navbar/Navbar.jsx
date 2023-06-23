afe3a75470730b4a175ec9e9a1e61f24adb38c5d
Netson Cavina <netsoncavina@gmail.com>
2023-06-22 21:07:01 -0300

    Desenvolvimento navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import "./navbar.css";
+import Notification from "../../img/notification.svg";
 
 export const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Notification App</span>
+      <div className="icons">
+        <div className="icon">
+          <img src={Notification} className="iconImg" alt="" />
+          <div className="counter">2</div>
+        </div>
+      </div>
+    </div>
+  );
 };
