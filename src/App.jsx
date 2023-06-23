2d47a6a834c660090ea39178b0bd8adc94d5b370
Netson Cavina <netsoncavina@gmail.com>
2023-06-23 04:34:09 -0300

    Adicionado conexão com o socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 import { Navbar } from "./components/navbar/Navbar";
 import { Card } from "./components/card/Card";
 import { posts } from "./data";
 function App() {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
+    console.log(socket);
+  }, []);
 
   // console.log(user);
   return (
