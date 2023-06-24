06aa075b2373e4fa7e50d90c914564f2f4053109
Netson Cavina <netsoncavina@gmail.com>
2023-06-24 03:31:15 -0300

    Correção de bugs

--- index.js
+++ index.js
@@ -6,10 +6,39 @@ const io = new Server({
   },
 });
 console.log("socket server started");
+
+let onlineUsers = [];
+
+const addNewUser = (userName, socketId) => {
+  !onlineUsers.some((user) => user.userName === userName) &&
+    onlineUsers.push({ userName, socketId });
+};
+
+const deleteUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userName) => {
+  return onlineUsers.find((user) => user.userName === userName);
+};
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("newUser", (userName) => {
+    addNewUser(userName, socket.id);
+    console.log(onlineUsers);
+    // io.emit("getUsers", onlineUsers);
+  });
+
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    console.log(senderName, receiverName, type);
+    console.log(receiver);
+    io.to(receiver.socketId).emit("getNotification", { senderName, type });
+  });
 
   socket.on("disconnect", () => {
+    deleteUser(socket.id);
     console.log("user disconnected");
   });
 });
